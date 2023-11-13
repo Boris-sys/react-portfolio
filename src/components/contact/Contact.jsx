e98dd6bbf26d05a9ee065040afb6a86a2e87a895
Boris <borissicsac@gmail.com>
2023-11-13 11:51:43 -0300

    contact styling and emailjs import

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 import './contact.css'
 import{MdOutlineEmail} from 'react-icons/md'
 import {BsLinkedin, BsWhatsapp} from 'react-icons/bs'
+import {useRef} from 'react'
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_key', 'template_key', form.current, 'api_key')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -12,22 +27,22 @@ export const Contact = () => {
       <div className='container contact__container'>
         <div className='contact__options'>
 
-          <article className='contact__options'>
-            <MdOutlineEmail/>
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>boriskerbs@gmail.com</h5>
             <a href="mailto:boriskerbs@gmail.com" target='_blank'>Write a mail</a>
           </article>
 
-          <article className='contact__options'>
-            <BsLinkedin/>
+          <article className='contact__option'>
+            <BsLinkedin className='contact__option-icon'/>
             <h4>LinkedIn</h4>
             <h5>Boris Kerbs</h5>
             <a href="https://www.linkedin.com/in/boris-kerbs/" target='_blank'>Send a message</a>
           </article>
 
-          <article className='contact__options'>
-            <BsWhatsapp/>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+54 3434806931</h5>
             <a href="https://api.whatsapp.com/send?phone=543434806931" target='_blank'>Chat with me</a>
@@ -35,7 +50,7 @@ export const Contact = () => {
 
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Your Full Name' required/>
           <input type="email" name="email" placeholder='Your Email' required/>
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
